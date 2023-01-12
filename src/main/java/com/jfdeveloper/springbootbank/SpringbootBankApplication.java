b06a5e50c5ef3c01d5aa37c905384b2b714ac233
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-01-11 21:26:51 -0500

    added project

---
+++ src/main/java/com/jfdeveloper/springbootbank/SpringbootBankApplication.java
@@ -0,0 +1,13 @@
+package com.jfdeveloper.springbootbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBankApplication.class, args);
+	}
+
+}
