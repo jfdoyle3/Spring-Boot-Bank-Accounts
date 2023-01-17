2835ac000378dfaa576bba2bbfdf0a4cce296a4e
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-01-17 06:54:49 -0500

    updated classes Account,Checking,Savings

--- src/main/java/com/jfdeveloper/springbootbank/entities/Account.java
+++ src/main/java/com/jfdeveloper/springbootbank/entities/Account.java
@@ -1,4 +1,33 @@
 package com.jfdeveloper.springbootbank.entities;
 
-public class Account {
+public abstract class Account {
+
+    private String name;
+
+    private String accountNumber;
+    protected double balance;
+
+    public Account() {
+    }
+
+    public Account(String name, String accountNumber) {
+        this.name=name;
+        this.accountNumber = accountNumber;
+        balance=0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract void deposit(double amount);
+
+    public abstract void withdraw(double amount);
 }
--- src/main/java/com/jfdeveloper/springbootbank/entities/Checking.java
+++ src/main/java/com/jfdeveloper/springbootbank/entities/Checking.java
@@ -1,4 +1,43 @@
 package com.jfdeveloper.springbootbank.entities;
 
-public class Checking {
+public class Checking extends Account{
+
+    private static double FEE=2.5;
+
+
+
+    public Checking(String name, String  accountNumber,double fee) {
+        super(name,accountNumber);
+        FEE=fee;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if(amount>0){
+            balance+=amount;
+            System.out.printf("Amount %.2f deposited%n",amount);
+            balance-=FEE;
+            System.out.printf("Fee %.2f Applied%n",FEE);
+            System.out.printf("Current Balance is: %.2f%n",balance);
+        }
+        else {
+            System.out.println("A negative amount cannot be deposited");
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if(amount>0){
+            if((amount+FEE)<=balance){
+                System.out.printf("Amount %.2f withdrawn from Account%n",amount);
+                balance-=amount;
+                balance-=FEE;
+                System.out.printf("Fee %.2f Applied%n",FEE);
+                System.out.printf("Current Balance is: %.2f%n",balance);
+            }
+        }
+        else {
+            System.out.println("Negative amount cannot be withdrawn");
+        }
+    }
 }
