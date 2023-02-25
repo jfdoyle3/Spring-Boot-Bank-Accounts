01b7cb7835aacc58bea05ae02b9740b5e3c2a3d4
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-02-25 03:56:03 -0500

    delete repositories

--- src/main/java/com/jfdeveloper/springbootbank/repositories/CheckingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jfdeveloper.springbootbank.repositories;
-
-import com.jfdeveloper.springbootbank.entities.Checking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CheckingRepository extends JpaRepository<Checking, Long> {
-}
