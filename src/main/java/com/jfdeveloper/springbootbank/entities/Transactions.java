8557ebdc9b621b57a071764c7c5c67f6fc394813
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-02-27 19:21:12 -0500

    added Transactions constructor and getter/setter

--- src/main/java/com/jfdeveloper/springbootbank/entities/Transactions.java
+++ src/main/java/com/jfdeveloper/springbootbank/entities/Transactions.java
@@ -7,11 +7,25 @@ import jakarta.persistence.*;
 public class Transactions extends Account {
 
 
-
     private String transactionType;
 
     public Transactions() {
     }
+
+    public Transactions(String name, long accountNumber, String transactionType) {
+        super(name, accountNumber);
+        this.transactionType = transactionType;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+
 
     @Override
     public void deposit(int amount) {
