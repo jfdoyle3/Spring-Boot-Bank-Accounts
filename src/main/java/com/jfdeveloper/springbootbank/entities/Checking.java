f3e3c6660593b7393ead588be0d820c05eeb74a5
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-03-01 22:03:47 -0500

    switched back to Single Table inheritance method

--- src/main/java/com/jfdeveloper/springbootbank/entities/Checking.java
+++ src/main/java/com/jfdeveloper/springbootbank/entities/Checking.java
@@ -2,9 +2,10 @@ package com.jfdeveloper.springbootbank.entities;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-@DiscriminatorValue("checking")
+// @DiscriminatorValue("checking")
 public class Checking extends Account{
 
     public Checking() {
--- src/main/java/com/jfdeveloper/springbootbank/entities/Savings.java
+++ src/main/java/com/jfdeveloper/springbootbank/entities/Savings.java
@@ -5,7 +5,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("savings")
+// @DiscriminatorValue("savings")
 public class Savings extends Account {
 
     public Savings() {
